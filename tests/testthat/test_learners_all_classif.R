0e0ed3a0db23a31bd0337a3975fb653f2ed28b84
Steve <stevo15025@gmail.com>
2017-02-28 16:07:51 -0500

    Broke classif all into mulitple tests. 87 learners are broken into sets of 20.

--- tests/testthat/test_learners_all_classif.R
+++ tests/testthat/test_learners_all_classif.R
@@ -1,15 +1,15 @@
 context("learners_all_classif")
 
-test_that("learners work: classif ", {
+test_that("learners work: classif 1-20 ", {
 
   # settings to make learners faster and deal with small data size
   hyperpars = list(
     classif.boosting = list(mfinal = 2L),
     classif.cforest = list(mtry = 1L),
     classif.bartMachine = list(verbose = FALSE, run_in_sample = FALSE,
-      # without this (and despite use_missing_data being TRUE), the test with missing data fails with a null point exception, which manifests itself as a completely different rJava error in the test
-      replace_missing_data_with_x_j_bar = TRUE,
-      num_iterations_after_burn_in = 10L),
+                               # without this (and despite use_missing_data being TRUE), the test with missing data fails with a null point exception, which manifests itself as a completely different rJava error in the test
+                               replace_missing_data_with_x_j_bar = TRUE,
+                               num_iterations_after_burn_in = 10L),
     classif.bdk = list(ydim = 2L),
     classif.earth = list(degree = 3L, nprune = 2L),
     classif.gbm = list(bag.fraction = 1, n.minobsinnode = 1),
@@ -22,10 +22,120 @@ test_that("learners work: classif ", {
 
   # binary classif
   task = subsetTask(binaryclass.task, subset = c(10:20, 180:190),
-    features = getTaskFeatureNames(binaryclass.task)[12:15])
-  lrns = mylist(task, create = TRUE)
+                    features = getTaskFeatureNames(binaryclass.task)[12:15])
+  lrns = mylist(task,subset = 1:20, create = TRUE)
   lapply(lrns, testThatLearnerParamDefaultsAreInParamSet)
   lapply(lrns, testBasicLearnerProperties, task = task, hyperpars = hyperpars)
+})
+
+test_that("learners work: classif 21-40 ", {
+
+  # settings to make learners faster and deal with small data size
+  hyperpars = list(
+    classif.boosting = list(mfinal = 2L),
+    classif.cforest = list(mtry = 1L),
+    classif.bartMachine = list(verbose = FALSE, run_in_sample = FALSE,
+                               # without this (and despite use_missing_data being TRUE), the test with missing data fails with a null point exception, which manifests itself as a completely different rJava error in the test
+                               replace_missing_data_with_x_j_bar = TRUE,
+                               num_iterations_after_burn_in = 10L),
+    classif.bdk = list(ydim = 2L),
+    classif.earth = list(degree = 3L, nprune = 2L),
+    classif.gbm = list(bag.fraction = 1, n.minobsinnode = 1),
+    classif.lssvm = list(kernel = "rbfdot", reduced = FALSE),
+    classif.nodeHarvest = list(nodes = 100L, nodesize = 5L),
+    classif.xyf = list(ydim = 2L),
+    classif.h2o.deeplearning = list(hidden = 2L, seed = getOption("mlr.debug.seed"), reproducible = TRUE),
+    classif.h2o.randomForest = list(seed = getOption("mlr.debug.seed"))
+  )
+
+  # binary classif
+  task = subsetTask(binaryclass.task, subset = c(10:20, 180:190),
+                    features = getTaskFeatureNames(binaryclass.task)[12:15])
+  lrns = mylist(task,subset = 21:40, create = TRUE)
+  lapply(lrns, testThatLearnerParamDefaultsAreInParamSet)
+  lapply(lrns, testBasicLearnerProperties, task = task, hyperpars = hyperpars)
+})
+
+test_that("learners work: classif 41-61", {
+
+  # settings to make learners faster and deal with small data size
+  hyperpars = list(
+    classif.boosting = list(mfinal = 2L),
+    classif.cforest = list(mtry = 1L),
+    classif.bartMachine = list(verbose = FALSE, run_in_sample = FALSE,
+                               # without this (and despite use_missing_data being TRUE), the test with missing data fails with a null point exception, which manifests itself as a completely different rJava error in the test
+                               replace_missing_data_with_x_j_bar = TRUE,
+                               num_iterations_after_burn_in = 10L),
+    classif.bdk = list(ydim = 2L),
+    classif.earth = list(degree = 3L, nprune = 2L),
+    classif.gbm = list(bag.fraction = 1, n.minobsinnode = 1),
+    classif.lssvm = list(kernel = "rbfdot", reduced = FALSE),
+    classif.nodeHarvest = list(nodes = 100L, nodesize = 5L),
+    classif.xyf = list(ydim = 2L),
+    classif.h2o.deeplearning = list(hidden = 2L, seed = getOption("mlr.debug.seed"), reproducible = TRUE),
+    classif.h2o.randomForest = list(seed = getOption("mlr.debug.seed"))
+  )
+
+  # binary classif
+  task = subsetTask(binaryclass.task, subset = c(10:20, 180:190),
+                    features = getTaskFeatureNames(binaryclass.task)[12:15])
+  lrns = mylist(task,subset = 41:60, create = TRUE)
+  lapply(lrns, testThatLearnerParamDefaultsAreInParamSet)
+  lapply(lrns, testBasicLearnerProperties, task = task, hyperpars = hyperpars)
+})
+
+
+test_that("learners work: classif 61-87", {
+
+  # settings to make learners faster and deal with small data size
+  hyperpars = list(
+    classif.boosting = list(mfinal = 2L),
+    classif.cforest = list(mtry = 1L),
+    classif.bartMachine = list(verbose = FALSE, run_in_sample = FALSE,
+                               # without this (and despite use_missing_data being TRUE), the test with missing data fails with a null point exception, which manifests itself as a completely different rJava error in the test
+                               replace_missing_data_with_x_j_bar = TRUE,
+                               num_iterations_after_burn_in = 10L),
+    classif.bdk = list(ydim = 2L),
+    classif.earth = list(degree = 3L, nprune = 2L),
+    classif.gbm = list(bag.fraction = 1, n.minobsinnode = 1),
+    classif.lssvm = list(kernel = "rbfdot", reduced = FALSE),
+    classif.nodeHarvest = list(nodes = 100L, nodesize = 5L),
+    classif.xyf = list(ydim = 2L),
+    classif.h2o.deeplearning = list(hidden = 2L, seed = getOption("mlr.debug.seed"), reproducible = TRUE),
+    classif.h2o.randomForest = list(seed = getOption("mlr.debug.seed"))
+  )
+
+  # binary classif
+  task = subsetTask(binaryclass.task, subset = c(10:20, 180:190),
+                    features = getTaskFeatureNames(binaryclass.task)[12:15])
+  lrns = mylist(task,subset = 61:87, create = TRUE)
+  lapply(lrns, testThatLearnerParamDefaultsAreInParamSet)
+  lapply(lrns, testBasicLearnerProperties, task = task, hyperpars = hyperpars)
+})
+
+test_that("learners work: classif 3", {
+
+  # settings to make learners faster and deal with small data size
+  hyperpars = list(
+    classif.boosting = list(mfinal = 2L),
+    classif.cforest = list(mtry = 1L),
+    classif.bartMachine = list(verbose = FALSE, run_in_sample = FALSE,
+                               # without this (and despite use_missing_data being TRUE), the test with missing data fails with a null point exception, which manifests itself as a completely different rJava error in the test
+                               replace_missing_data_with_x_j_bar = TRUE,
+                               num_iterations_after_burn_in = 10L),
+    classif.bdk = list(ydim = 2L),
+    classif.earth = list(degree = 3L, nprune = 2L),
+    classif.gbm = list(bag.fraction = 1, n.minobsinnode = 1),
+    classif.lssvm = list(kernel = "rbfdot", reduced = FALSE),
+    classif.nodeHarvest = list(nodes = 100L, nodesize = 5L),
+    classif.xyf = list(ydim = 2L),
+    classif.h2o.deeplearning = list(hidden = 2L, seed = getOption("mlr.debug.seed"), reproducible = TRUE),
+    classif.h2o.randomForest = list(seed = getOption("mlr.debug.seed"))
+  )
+
+  # binary classif
+  task = subsetTask(binaryclass.task, subset = c(10:20, 180:190),
+                    features = getTaskFeatureNames(binaryclass.task)[12:15])
 
   # binary classif with factors
   lrns = mylist("classif", properties = "factors", create = TRUE)
@@ -38,19 +148,19 @@ test_that("learners work: classif ", {
   # binary classif with prob
   lrns = mylist(binaryclass.task, properties = "prob", create = TRUE)
   lapply(lrns, testBasicLearnerProperties, task = binaryclass.task,
-    hyperpars = hyperpars, pred.type = "prob")
+         hyperpars = hyperpars, pred.type = "prob")
 
   # binary classif with weights
   lrns = mylist("classif", properties = "weights", create = TRUE)
   lapply(lrns, testThatLearnerRespectsWeights, hyperpars = hyperpars,
-    task = binaryclass.task, train.inds = binaryclass.train.inds, test.inds = binaryclass.test.inds,
-    weights = rep(c(10000L, 1L), c(10L, length(binaryclass.train.inds) - 10L)),
-    pred.type = "prob", get.pred.fun = getPredictionProbabilities)
+         task = binaryclass.task, train.inds = binaryclass.train.inds, test.inds = binaryclass.test.inds,
+         weights = rep(c(10000L, 1L), c(10L, length(binaryclass.train.inds) - 10L)),
+         pred.type = "prob", get.pred.fun = getPredictionProbabilities)
 
   # classif with missing
   lrns = mylist("classif", properties = "missings", create = TRUE)
   lapply(lrns, testThatLearnerHandlesMissings, task = task, hyperpars = hyperpars)
-  
+
   # classif with oobpreds
   lrns = mylist("classif", properties = "oobpreds", create = TRUE)
   lapply(lrns, testThatGetOOBPredsWorks, task = task)
